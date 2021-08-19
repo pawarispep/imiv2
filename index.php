98f4fa1602b3a1534c6de792f948b614f75242ef
pawarispep <pawaris.ko@mail.wu.ac.th>
2021-08-19 11:34:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,40 @@
-Pawaris Kongjan
+<!doctype html>
+<html lang="en">
+  <head>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+    <script src="https://code.jquery.com/jquery-2.2.4.js"
+    integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI=" crossorigin="anonymous"></script>
+  </head>
+
+  <body>
+    <h1>Hello, world!</h1>
+    <div class="row">
+        <div class="col-4">
+            <iframe src="https://thingspeak.com/channels/1458413/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" frameborder="0" class="src"></iframe>
+        </div>
+        <div class="col-4"></div>
+        <div class="col-4"></div>
+
+    </div>
+    
+
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+
+    
+  </body>
+  <script>
+      $(()=>{
+          alert("Hello");
+      })
+
+
+  </script>
+</html>
