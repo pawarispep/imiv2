6e69514d1bfe8ab4425eabe75e25924820efdd95
pawarispep <pawaris.ko@mail.wu.ac.th>
2021-08-20 13:22:32 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,9 @@
 <!doctype html>
 <html lang="en">
   <head>
-    
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
-
     <title>Hello, world!</title>
     <script src="https://code.jquery.com/jquery-2.2.4.js"
     integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI=" crossorigin="anonymous"></script>
@@ -15,12 +11,7 @@
 
   <body>
     <h1>Hello world!</h1>
-    
-
-    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
-
-    
   </body>
   <script>
       $(()=>{
@@ -29,12 +20,10 @@
           $.getJSON(url)
             .done(function(data){
               console.log(data);
+              console.log(f[1]);
             })
             .fail(function(error){
-
             });
       });
-
-
   </script>
 </html>
