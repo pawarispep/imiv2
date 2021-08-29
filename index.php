a3bd1c60c9e85a35d4af7eb267ff307944ae0cef
pawarispep <pawaris.ko@mail.wu.ac.th>
2021-08-29 20:00:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -145,8 +145,8 @@
                   light_status="light";
                 }
                 
-              $("#lastTemperature").text(feed[v].field2+" C");
-              $("#lastHumidity").text(feed[v].field1+" %");
+              $("#lastTemperature").text(feed[0].field2+" C");
+              $("#lastHumidity").text(feed[0].field1+" %");
               $("#lightStatus").text(light_status);
               $("#lastUpdate").text(dateStr);
 
